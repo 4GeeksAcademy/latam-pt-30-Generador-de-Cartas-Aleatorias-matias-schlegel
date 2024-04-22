731581db8b72ae9b5b3c39f406df6f0ba52bb485
matischlegel <161637922+matischlegel@users.noreply.github.com>
2024-04-22 18:43:43 +0000

    proyecto en proceso

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let icons = ["♦", "♥", "♠", "♣"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = function getRandomIcon() {
+  let random = icons[Math.floor(Math.random() * icons.length)];
+  document.querySelector("#random").innerHTML = random;
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let randomIcon2 = icons[Math.floor(Math.random() * icons.length)];
+  document.querySelector("#randomIcon2").innerHTML = random;
 };
+
+// ya generamos el icono aleatorio
+// ahora hay que generar el número aleatorio
+
+function getRandomInt(min, max) {
+  // Genera dos valores aleatorios(especificando el parametro deseado)
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+let numero = getRandomInt(2, 11); // Excluye el último número, (no sé por qué)
+document.querySelector("#numeroAleatorio").innerHTML = numero;
